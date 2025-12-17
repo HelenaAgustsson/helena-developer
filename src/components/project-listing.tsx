aaaf8bc1bdffda6e0a91c1bceb24119c1a602120
Helena Agustsson <agustssonhelena@gmail.com>
2025-12-17 17:31:06 +0000

    added images to project component

--- src/components/project-listing.tsx
+++ src/components/project-listing.tsx
@@ -2,20 +2,39 @@ import { PortableText } from "next-sanity";
 import { PROJECT_QUERYResult } from "@/sanity/types";
 import Categories from "./categories";
 import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
 
 interface ProjectListingProps {
     data: NonNullable<PROJECT_QUERYResult[0]>
 }
 
 const ProjectListing = ({data}: ProjectListingProps) => {
-    const {project, github, body, categories } = data;
+    const {project, github, body, categories, mainImage } = data;
     return (
-        <li className="block my-4 text-aqua">
-            {project}
-            <br />
-            <div className="prose text-mist"> {body ? <PortableText value={body} /> :null}</div>
-            <Link href={github}>Github repo</Link>
-            {categories ? <Categories categories={categories} />:null}
+        <li className="flex flex-col md:flex-row block my-4 text-aqua">
+            <div className="mb-5">
+                {mainImage ? (
+                <img
+                    className="aspect-320/180"
+                    src={urlFor(mainImage)
+                        .width(320)
+                        .height(180)
+                        .quality(80)
+                        .auto("format")
+                        .url()}
+                    alt={mainImage?.alt || ""}
+                    width="320"
+                    height="180"
+                />
+            ):null}
+            </div>
+            <div className="md:ml-5">
+                {project}
+                <br />
+                <div className="prose text-mist"> {body ? <PortableText value={body} /> :null}</div>
+                    <Link href={github}>Github repo</Link>
+                    {categories ? <Categories categories={categories} />:null}
+            </div>
         </li>
     )
 }
