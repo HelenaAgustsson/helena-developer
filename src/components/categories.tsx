048ac40380cc7326fbe1f08b8eca8f09f5fac4e3
Helena Agustsson <agustssonhelena@gmail.com>
2025-12-18 17:05:38 +0000

    styled project listing

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -5,7 +5,7 @@ interface CategoryProps {
 
 const Categories = ({categories}:CategoryProps ) => {
     return (
-        <ul className="mt-2 flex flex-wrap">
+        <ul className="my-2 flex flex-wrap">
             {categories.map((cat, index) => (
                 <li key={index} className="mr-1.5 mt-2">
                      <div className="bg-thistle text-midnight-violet flex items-center rounded-full px-3 py-1 text-xs font-medium leading-5">{cat}</div>
--- src/components/project-listing.tsx
+++ src/components/project-listing.tsx
@@ -30,10 +30,9 @@ const ProjectListing = ({data}: ProjectListingProps) => {
             <div className="">
                 {title}
                 <br />
-                 <Link href={link}>{formattedLink}</Link>
+                <Link href={link}>{formattedLink}</Link>
                 <div className="prose text-mist"> {body ? <PortableText value={body} /> :null}</div>
-                {categories ? <Categories categories={categories} />:null}
-                <div className="mt-5"><Link href={github}><Image src={githubLogo} alt="github logo" height="25" /></Link></div>
+                <div className="mt-2"><Link href={github}><Image src={githubLogo} alt="github logo" height="25" /></Link></div>
             </div>
         </li>
     )
