82753f2a22f1f33f413da71b2e9ac5d22828e194
Helena Agustsson <agustssonhelena@gmail.com>
2025-12-24 15:32:45 +0000

    intro background

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -8,7 +8,7 @@ const Categories = ({categories}:CategoryProps ) => {
         <ul className="my-2 flex flex-wrap">
             {categories.map((cat, index) => (
                 <li key={index} className="mr-1.5 mt-2">
-                     <div className="bg-thistle text-midnight-violet flex items-center rounded-full px-3 py-1 text-xs font-medium leading-5">{cat}</div>
+                     <div className="bg-thistle text-violet flex items-center rounded-full px-3 py-1 text-xs font-medium leading-5">{cat}</div>
                 </li>
                 )
             )}
