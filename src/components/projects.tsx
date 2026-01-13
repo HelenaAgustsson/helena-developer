e80fb9f6d7bc4999d9f2ff9f799ef5b7c586c575
Helena Agustsson <agustssonhelena@gmail.com>
2026-01-13 16:49:49 +0000

    added highlighted styling project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,20 +1,21 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { PROJECT_QUERY } from "@/sanity/lib/queries";
 import ProjectListing from "./project-listing";
+import { getHighlighted } from "./helpers";
 
 const options = { next: { revalidate: 60 } };
 
 export default async function Projects() {
   const { data: posts } = await sanityFetch({ query: PROJECT_QUERY });
-  
+
   return (
     <section id="projects" className="mb-20">
-       <h3 className="pt-10 text-xl font-bold">Projects</h3>
-        <ul className="grid grid-cols-1">
-          {posts.map((post, index) => (
-           <ProjectListing key={index} data={post} />
-          ))}
-        </ul>
+      <h3 className="pt-10 text-xl font-bold">Projects</h3>
+      <ul className="grid grid-cols-1">
+        {posts.map((post, index) => (
+          <ProjectListing key={index} data={post} highlighted={getHighlighted(index)} />
+        ))}
+      </ul>
     </section>
   );
 }
