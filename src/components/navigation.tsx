5daefd68892498724d9b0f4439f2032b1cad3059
Helena Agustsson <agustssonhelena@gmail.com>
2026-03-17 12:19:00 +0000

    refactor nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,34 +1,27 @@
 
 const Navigation = () => {
+
+    const items = [
+        { title: 'About me', anchor: '#about' },
+        { title: 'Experience', anchor: '#experience' },
+        { title: 'Projects', anchor: '#projects' },
+        { title: 'Education', anchor: '#education' }]
+
     return (
-            <nav className="nav hidden mt-10 lg:block">
-                <ul className="w-max">
-                    <li>
-                        <a href="#about" className="group flex items-center py-3 target:text-aqua hover:text-aqua hover:[&>div]:bg-aqua active">
-                            <div className="mr-4 h-px w-8 bg-mist hover:bg-aqua"></div>
-                            <span className="nav-text text-xs font-bold uppercase tracking-widest">About me</span>    
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#experience" className="group flex items-center py-3 hover:text-aqua hover:[&>div]:bg-aqua">
-                            <div className="mr-4 h-px w-8 bg-mist"></div>
-                            <span className="nav-text text-xs font-bold uppercase tracking-widest">Experience</span>
-                        </a>
+        <nav className="nav hidden mt-10 lg:block">
+            <ul className="w-max">
+                {items.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a href={item.anchor} className="group flex items-center py-3 target:text-aqua hover:text-aqua hover:[&>div]:bg-aqua active">
+                                <div className="mr-4 h-px w-8 bg-mist hover:bg-aqua"></div>
+                                <span className="nav-text text-xs font-bold uppercase tracking-widest">{item.title}</span>
+                            </a>
                         </li>
-                    <li>
-                        <a href="#projects" className="group flex items-center py-3 hover:text-aqua hover:[&>div]:bg-aqua">
-                            <div className="mr-4 h-px w-8 bg-mist"></div>
-                             <span className="nav-text text-xs font-bold uppercase tracking-widest">Projects</span>    
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#education" className="group flex items-center py-3 hover:text-aqua hover:[&>div]:bg-aqua">
-                            <div className="mr-4 h-px w-8 bg-mist"></div>
-                             <span className="nav-text text-xs font-bold uppercase tracking-widest">Education</span>    
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+                    )
+                })}
+            </ul>
+        </nav>
 
     )
 }
