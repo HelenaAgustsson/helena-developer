bf56c8fe7df4392245caabc55a41191050231817
Helena Agustsson <agustssonhelena@gmail.com>
2026-03-17 12:20:26 +0000

    named image file

--- src/components/projects/project-listing.tsx
+++ src/components/projects/project-listing.tsx
@@ -1,7 +1,7 @@
 import { PortableText } from "next-sanity";
 import { PROJECT_QUERYResult } from "@/sanity/types";
 import Image from "next/image";
-import { CustomImage } from "../customImage";
+import { CustomImage } from "../custom-image";
 import Categories from "../categories";
 import externalLink from "../icons/externalLink.svg"
 import externalLinkWhite from "../icons/externalLinkWhite.svg"
