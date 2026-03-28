cf523f3f2b66075a9171f4dffa9c5fcfe55ca491
Helena Agustsson <agustssonhelena@gmail.com>
2026-03-28 15:25:17 +0000

    jobs unit test

---
+++ src/components/jobs/jobs.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { sanityFetch } from '@/sanity/lib/live';
+import { Jobs } from './jobs';
+
+jest.mock("next-sanity", () => ({
+    defineQuery: (query: string) => query,
+}));
+
+// mock the sanityFetch function to return a resolved promise with mock data
+jest.mock("@/sanity/lib/live", () => ({
+    sanityFetch: jest.fn(),
+}));
+
+jest.mock("./jobListing", () => ({
+    JobListing: ({ data }: any) => <li data-testid="job-item">{data.title}</li>,
+}));
+
+describe('Jobs', () => {
+    it('renders job section correctly', async () => {
+        const mockPosts = [
+            { title: "Frontend Developer" },
+            { title: "Conversion Optimizer" },
+        ];
+
+        (sanityFetch as jest.Mock).mockResolvedValue({
+            data: mockPosts,
+        });
+
+        const component = await Jobs();
+        render(component);
+
+        expect(screen.getByText("Frontend Developer")).toBeInTheDocument();
+    })
+
+});
